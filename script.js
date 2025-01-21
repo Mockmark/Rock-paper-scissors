2786d9762b5a731c16097323dd1376e8a6ab4c36
Michael Mark <selfdevourment@gmail.com>
2025-01-21 11:15:30 +0500

    Add to getHumanChoice toLowerCase.

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const choiceSpan = 3
+
+//create function for the PC's choice
+
 function getComputerChoice(max) {
-    let CompChoice = Math.floor(Math.random() * max);
-    return CompChoice
+    let compChoice = Math.floor(Math.random() * max);
+    return compChoice
 }
+
+//create function for the user's choice
 
 let HumChoice
 
-function getHumanChoice() {
-    HumChoice = prompt("Make your choice:")
-    return HumChoice
+function getHumanChoice(arg) {
+    arg = prompt("Make your choice:")
+    arg = arg.toLowerCase()
+    return arg
+}
+
+let humanScore = 0
+let computerScore = 0
+
+//function for a single game round
+
+function playRound(humChoice, compChoice) {
+
 }
 
 console.log(getComputerChoice(choiceSpan))
-console.log(getHumanChoice())
+console.log(getHumanChoice(HumChoice))
